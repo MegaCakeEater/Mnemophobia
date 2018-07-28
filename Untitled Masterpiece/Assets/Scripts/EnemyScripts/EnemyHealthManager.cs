fd307583ac6076fac4feea960c83062249aa1e78
MegaCakeEater <emil_ngaard@hotmail.com>
2018-07-28 10:05:16 +0200

    Moved stuff into folders, fixed sprint

---
+++ Untitled Masterpiece/Assets/Scripts/EnemyScripts/EnemyHealthManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("hit " + other);
+		if(other.gameObject.tag == "PlayerWeapon") {
+			Destroy(this.gameObject);
+		}
+	}
+}
